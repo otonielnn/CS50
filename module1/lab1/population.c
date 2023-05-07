811165d4c1e99232639934a8aeeab43341b461e2
Otoniel <79499426+otonielnn@users.noreply.github.com>
2023-05-07 10:11:10 +0000

    Sun, May 7, 2023, 7:11 AM -03:00

---
+++ module1/lab1/population.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+     // TODO: Solicite o valor inicial ao usuário
+      int start, end, years = 0;
+      do
+      {
+        start = get_int("Start size: ");
+      }
+      while (start < 1);
+
+
+     // TODO: Solicite o valor final ao usuário
+      end = get_int("End size: ");
+
+     // TODO: Calcule o número de anos até o limite
+     while (start < end)
+     {
+        start += start / 3;
+        start -= start / 4;
+        years += 1;
+     }
+
+     // TODO: Imprima o número de anos
+     printf("%i\n", years);
+}
