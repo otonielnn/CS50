df86439ca39ba817f6bf95af1d2d03650b8875d6
Otoniel <79499426+otonielnn@users.noreply.github.com>
2023-05-07 07:32:41 +0000

    Sun, May 7, 2023, 7:32 AM +00:00

--- module1/cash/cash.c
+++ module1/cash/cash.c
@@ -1,41 +1,26 @@
-#include <stdio.h>
-#include <cs50.h>
-#include <math.h>
-
-int main(void)
-{
-    float valor = -1;
-    int cents = round(valor * 100), coins = 0;
-
-    while (valor < 0)
-    {
-        valor = get_float("Valor: ");
-    }
-    printf("%i\n", cents);
+ float n = get_positive_float();
+    int cents = round(n * 100), coins = 0;
+    // Passing on money owed
     do
     {
         if (cents >= 25)
         {
             cents -= 25;
-            coins += 1;
         }
         else if (cents >= 10)
         {
             cents -= 10;
-            coins += 1;
         }
         else if (cents >= 5)
         {
             cents -= 5;
-            coins += 1;
         }
         else
         {
             cents -= 1;
-            coins += 1;
         }
-        printf("%i\n", cents);
+        coins++;
     }
     while (cents > 0);
-    printf("%i\n", coins);
-}
+    // Show amount of coins
+    printf("coins: %i\n", coins);
