32ac53ef0f472319a8006ade70c2dbd53d011b3a
Otoniel <79499426+otonielnn@users.noreply.github.com>
2023-05-07 06:04:39 +0000

    Sun, May 7, 2023, 6:04 AM +00:00

---
+++ mario/more/mario.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <cs50.h>
+
+void torre(int n);
+
+int main(void)
+{
+    int n = 0;
+    while (n < 1 || n > 8)
+    {
+        n = get_int("Height: ");
+    }
+    torre(n);
+}
+
+void torre(int n)
+{
+    for (int l = 1; l <= n; l++)
+    {
+        for (int d = n; d > l; d--)
+        {
+            if (l != 0)
+            {
+                printf(" ");
+            }
+        }
+
+        for (int h = 1; h <= l; h++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
