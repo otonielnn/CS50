2204ae6304323d1e2b46459ac0a1282cee167a21
Otoniel <79499426+otonielnn@users.noreply.github.com>
2023-05-07 06:57:06 +0000

    Sun, May 7, 2023, 6:57 AM +00:00

--- module1/cash/cash.c
+++ module1/cash/cash.c
@@ -12,33 +12,32 @@ int main(void)
         valor = get_float("Valor: ");
     }
 
-    while (valor > 0.25)
+    while (valor >= 0.25)
     {
         valor -= 0.25;
         total_coins += 1;
         printf("%i\n", total_coins); // OK - 1
     }
 
-    while (valor > 0.10)
+    while (valor >= 0.10)
     {
         valor -= 0.10;
         total_coins += 1;
         printf("%i\n", total_coins); //OK - 2
     }
 
-    while (valor > 0.05)
+    while (valor >+ 0.05)
     {
         valor -= 0.05;
         total_coins += 1;
         printf("%i\n", total_coins); // - 3
     }
-    printf("%f\n", valor);
-    while (valor > 0.01)
+    while (valor >= 0.01)
     {
         valor -= 0.01;
         total_coins += 1;
+    printf("%f\n", valor);
         printf("%i\n", total_coins); // - 3
     }
-    printf("%f\n", valor);
     printf("%i\n", total_coins);
 }
