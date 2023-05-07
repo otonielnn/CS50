eeb17f9384545ee7185c6bba3770fc5b478db376
Otoniel <79499426+otonielnn@users.noreply.github.com>
2023-05-07 10:24:55 +0000

    Sun, May 7, 2023, 7:24 AM -03:00

--- module1/lab1/population.c
+++ module1/lab1/population.c
@@ -3,31 +3,32 @@
 
 int main(void)
 {
-     // TODO: Solicite o valor inicial ao usuário
-      int start, end, years = 0;
-      do
-      {
+
+    // TODO: Solicite o valor inicial ao usuário
+    int start, end, years = 0;
+    do
+    {
         start = get_int("Start size: ");
-      }
-      while (start < 9);
+    }
+    while (start < 9);
 
 
-     // TODO: Solicite o valor final ao usuário
-     do
-     {
+    // TODO: Solicite o valor final ao usuário
+    do
+    {
         end = get_int("End size: ");
-     }
-     while (end <= start);
-     // TODO: Calcule o número de anos até o limite
-     while (start < end)
-     {
+    }
+    while (end <= start);
+    // TODO: Calcule o número de anos até o limite
+    while (start < end)
+    {
         int born = start / 3;
         int died = start / 4;
         start += born;
         start -= died;
         years += 1;
-     }
+    }
 
-     // TODO: Imprima o número de anos
-     printf("%i\n", years);
+    // TODO: Imprima o número de anos
+    printf("%i\n", years);
 }
