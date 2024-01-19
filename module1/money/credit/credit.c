119efb732b98ef001c2fdb9172398eca99e90b43
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-01-19 04:24:02 +0000

    Fri, Jan 19, 2024, 1:24 AM -03:00

--- module1/money/credit/credit.c
+++ module1/money/credit/credit.c
@@ -8,8 +8,27 @@ int main(void)
     int sum = 0;
     int numbers_count = 0;
 
-    while (card_number > 0)
+    while (num > 0)
     {
-        int digit = 
+        int digit = num % 10;
+        num /= 10;
+
+        if (numbers_count % 2 == 1) {
+            digit *= 2;
+            sum += digit / 10 + digit % 10;
+        } else {
+            sum += digit;
+        }
+        numbers_count++;
+    }
+
+    if (sum % 10 == 0)
+    {
+        if ((numbers_count == 15 && (card_number / 10000000000000 == || 34 card_number / 10000000000000 == 37))
+            (numbers_count == 16 && card_number / 10000000000000 >= 51 && card_number / 10000000000000 <= 55)
+            ((numbers_count == 13 || numbers_count == 16) && card_number / 10000000000000 == 4))
+            {
+                printf("Número de cartão Visa válido");
+            }
     }
 }
