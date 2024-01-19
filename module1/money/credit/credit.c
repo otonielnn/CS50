32e0d6161d2c2ca6dfb585fd88c5ff530cb6ee11
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-01-19 05:01:43 +0000

    Fri, Jan 19, 2024, 2:01 AM -03:00

--- module1/money/credit/credit.c
+++ module1/money/credit/credit.c
@@ -1,41 +1,59 @@
-#include <cs50.h>
-#include <math.h>
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
-    long card_number = get_int("Número: ");
-    int sum = 0;
-    int numbers_count = 0;
+    long long cardNumber;
 
-    while (sum > 0)
+    do
     {
-        int digit = sum % 10;
-        sum /= 10;
+        cardNumber = get_long_long("Digite o número do cartão de crédito: ");
+    } while (cardNumber <= 0);
 
-        if (numbers_count % 2 == 1) {
+    int sum1 = 0, sum2 = 0;
+    int multiply = 0;
+
+    while (cardNumber > 0)
+    {
+        int digit = cardNumber % 10;
+
+        if (multiply)
+        {
             digit *= 2;
-            sum += digit / 10 + digit % 10;
-        } else {
-            sum += digit;
+            sum1 += digit / 10 + digit % 10;
         }
-        numbers_count++;
+        else
+        {
+            sum2 += digit;
+        }
+
+        multiply = !multiply;
+        cardNumber /= 10;
     }
 
-    if (sum % 10 == 0)
+    bool isValid = (sum1 + sum2) % 10 == 0;
+
+    if (isValid)
     {
-        if ((numbers_count == 16 || numbers_count == 13) && card_number / 10000000000000 == 4)
-            {
-                printf("VISA\n");
-            } else if (numbers_count == 16 && (card_number / 10000000000000 >= 51 || card_number / 10000000000000 <= 55))
-            {
-                printf("MASTERCARD\n");
-            } else if (numbers_count == 15 && (card_number / 10000000000000 == 34 || card_number / 10000000000000 == 37))
-            {
-                printf("AMEX\n");
-            } else
-            {
-                printf("INVALID\n");
-            }
+        if ((cardNumber == 34 || cardNumber == 37) && isValid)
+        {
+            printf("American Express\n");
+        }
+        else if ((cardNumber >= 51 && cardNumber <= 55) && isValid)
+        {
+            printf("MasterCard\n");
+        }
+        else if ((cardNumber / 1000000000000 == 4 || cardNumber / 100000000000000 == 4) && isValid)
+        {
+            printf("Visa\n");
+        }
+        else
+        {
+            printf("Cartão de crédito válido, mas a bandeira não foi identificada.\n");
+        }
+    }
+    else
+    {
+        printf("Cartão de crédito inválido!\n");
     }
 }
