6a0dcddb40f9f2e5365030fc2e2a9bffd76e903e
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-01-19 10:23:30 +0000

    Fri, Jan 19, 2024, 7:23 AM -03:00

--- module2/scores.c
+++ module2/scores.c
@@ -5,15 +5,15 @@ int main(void)
 {
     int total = get_int("Total number of scores: ");
     int scores[total];
-    float sum;
+    float sum = 0;
     for (int i = 0; i < total; i++)
     {
-        scores[i] = get_int("%dº Score: ", i);
+        scores[i] = get_int("%dº Score: ", i + 1);
     }
 
     for (int i = 0; i < total; i++)
     {
-        sum = scores[i];
+        sum += scores[i];
     }
 
     float avg = sum / total;
