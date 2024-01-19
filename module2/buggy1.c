5d3cc32aa550eb5b7bd7065bf06467e6f95c6c79
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-01-19 09:45:51 +0000

    Fri, Jan 19, 2024, 6:45 AM -03:00

--- module2/buggy1.c
+++ module2/buggy1.c
@@ -1,8 +1,12 @@
 #include <cs50.h>
 #include <stdio.h>
 
+// Prototype
+int get_negative_int(void);
+
 int main(void)
 {
+    // Get negative integer from user
     int i = get_negative_int();
     printf("%i\n");
 }
