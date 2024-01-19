a0c692807e0abcabaf226033df07f6a84c9fbd60
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-01-19 05:34:37 +0000

    Fri, Jan 19, 2024, 2:34 AM -03:00

--- module1/money/credit/credit.c
+++ module1/money/credit/credit.c
@@ -1,39 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 
-int luhnCheck(char* cardNumber) {
-    int len = strlen(cardNumber);
-    int sum = 0;
-    int isSecond = 0;
+int main(void) {
+    // Get card number
+    long n = get_long("Number: ");
 
-    // Percorre os dígitos do cartão de crédito da direita para a esquerda
-    for (int i = len - 1; i >= 0; i--) {
-        int digit = cardNumber[i] - '0';
-
-        if (isSecond) {
-            digit *= 2;
-            if (digit > 9) {
-                digit -= 9;
-            }
-        }
-
-        sum += digit;
-        isSecond = !isSecond;
+    // Count length
+    int i = 0;
+    long cc = n;
+    while (cc > 0)
+    {
+        cc = cc / 10;
+        i++;
     }
 
-    // O número é válido se a soma for divisível por 10
-    return (sum % 10 == 0);
-}
-
-int main() {
-    char cardNumber[20];
-
-    printf("Digite o número do cartão de crédito: ");
-    scanf("%s", cardNumber);
-
-    if (luhnCheck(cardNumber)) {
-        printf("O número do cartão é válido.\n");
-    } else {
-        printf("O número do cartão é inválido.\n");
-    }
+    
 }
