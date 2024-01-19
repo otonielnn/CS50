62ff6e606d803b4e3e7924aa256f22113e6ef4ac
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-01-19 04:44:44 +0000

    Fri, Jan 19, 2024, 1:44 AM -03:00

--- module1/money/credit/credit.c
+++ module1/money/credit/credit.c
@@ -25,7 +25,7 @@ int main(void)
     if (sum % 10 == 0)
     {
         if ((numbers_count == 15 && (card_number / 10000000000000 == 34 || card_number / 10000000000000 == 37)) &&
-            (numbers_count == 16 && card_number / 10000000000000 >= 51 && card_number / 10000000000000 <= 55) &&
+            (numbers_count == 16 && (card_number / 10000000000000 >= 51 && card_number / 10000000000000 <= 55)) &&
             ((numbers_count == 13 || numbers_count == 16) && card_number / 10000000000000 == 4))
             {
                 printf("VISA\n");
