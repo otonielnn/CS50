3b0ec3ad19028ad2a418e80f3cea8663957dd333
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-01-19 10:17:16 +0000

    Fri, Jan 19, 2024, 7:17 AM -03:00

--- module2/scores.c
+++ module2/scores.c
@@ -2,9 +2,11 @@
 
 int main(void)
 {
-    int score1 = get_int("");
-    int score2 = 73;
-    int score3 = 33;
+    int scores[3];
+    scores[0] = get_int("1º Score: ");
+    scores[1] = get_int("2º Score: ");
+    scores[2] = get_int("3º Score: ");
+    float sum = scores[0] + scores[1] + scores[2];
 
-    printf("Average: %f\n", (score1 + score2 + score3) / 3.0);
+    printf("Average: %f\n", avg / 3.);
 }
