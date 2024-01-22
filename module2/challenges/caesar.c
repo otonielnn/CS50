b7b824e7e4e42371185372829e42b01bec2f1d05
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-01-22 20:30:13 +0000

    Mon, Jan 22, 2024, 5:30 PM -03:00

--- module2/challenges/caesar.c
+++ module2/challenges/caesar.c
@@ -10,7 +10,7 @@ int main(int argc, string argv[])
     {
         for (int i = 0; i < strlen(argv[1]); i++)
         {
-            if (isdigit(argv[1][i] == false))
+            if (!isdigit(argv[1][i]))
             {
                 printf("Usage: ./caesar key\n");
                 return 1;
