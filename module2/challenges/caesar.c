2bcf48d169fc32e90390041d6443b0173705366e
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-01-22 19:19:47 +0000

    Mon, Jan 22, 2024, 4:19 PM -03:00

--- module2/challenges/caesar.c
+++ module2/challenges/caesar.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(void)
+int main(int argc, string argv[])
 {
     int key = get_int("Key: ");
     string text = get_string("Text: ");
     string encipher = "";
-    
+
 }
