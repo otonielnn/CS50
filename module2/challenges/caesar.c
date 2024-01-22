08c0754a13366678fd57e3f5bd3f1bcf4fd42155
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-01-22 19:43:08 +0000

    Mon, Jan 22, 2024, 4:43 PM -03:00

--- module2/challenges/caesar.c
+++ module2/challenges/caesar.c
@@ -2,16 +2,21 @@
 #include <ctype.h>
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, string argv[])
 {
     if (isdigit(*argv[1]))
     {
+        int key = atoi(argv[1]);
         string text = get_string("Text: ");
 
         for (int i = 0; i < strlen(text); i++)
         {
-            if (text[i])
+            if (text[i] >= 'a' && text[i] <= 'z')
+            {
+                printf("%c", (((s[i] - 'a') +)))
+            }
         }
     }
 }
