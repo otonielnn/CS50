bcc35e6880f5e766065eb82c0acd79cc6fbb46ba
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-01-22 20:22:33 +0000

    Mon, Jan 22, 2024, 5:22 PM -03:00

--- module2/challenges/caesar.c
+++ module2/challenges/caesar.c
@@ -18,15 +18,17 @@ int main(int argc, string argv[])
         }
 
         int key = atoi(argv[1]);
+        string text = "";
 
         if (!isdigit(key))
         {
             printf("Usage: ./caesar key\n");
             return 1;
         }
-        
-
-        string text = get_string("Text: ");
+        else
+        {
+            text = get_string("Text: ");
+        }
 
         printf("ciphertext: ");
 
