f3cfe6fe18de22343acc4c6706d472fdea240979
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-01-23 00:17:14 +0000

    Mon, Jan 22, 2024, 9:17 PM -03:00

--- module2/lab2/scrabble.c
+++ module2/lab2/scrabble.c
@@ -4,5 +4,5 @@ int main(void)
 {
     string player1 = get_string("Player 1: ");
     string player2 = get_string("Player 2: ");
-    
+    string letters = {"A" ,"B" ,"C" ,"D",E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z}
 }
