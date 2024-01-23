8b4837087f69cbf7cdfdcdfd5265e5397e632e79
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-01-23 00:28:09 +0000

    Mon, Jan 22, 2024, 9:28 PM -03:00

--- module2/lab2/scrabble.c
+++ module2/lab2/scrabble.c
@@ -1,16 +1,54 @@
 #include <cs50.h>
-#include <string.h>
+#include <stdio.h>
 #include <ctype.h>
+#include <string.h>
+
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+int compute_score(string word);
 
 int main(void)
 {
-    string player1 = get_string("Player 1: ");
-    string player2 = get_string("Player 2: ");
-    string letters[] = {"A" ,"B" ,"C" ,"D" ,"E" ,"F" ,"G" ,"H" ,"I" ,"J" ,"K" ,"L" ,"M" ,"N" ,"O" ,"P" ,"Q" ,"R" ,"S" ,"T" ,"U" ,"V" ,"W" ,"X" ,"Y" ,"Z"};
-    int score[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+    string word[2];
 
-    for (int i = 0; i < strlen(player1); i++)
+    word[0] = get_string("Player 1: ");
+    word[1] = get_string("Player 2: ");
+
+    int score1 = compute_score(word[0]);
+    int score2 = compute_score(word[1]);
+
+    if (score1 > score2)
     {
-        if (toupper(player1[i]) == 'A')
+        printf("Player 1 wins!");
     }
+
+    if (score1 < score2)
+    {
+        printf("Player 2 wins!");
+    }
+
+    else if (score1 == score2)
+    {
+        printf("Draw!");
+    }
+}
+
+int compute_score(string word)
+{
+    int score = 0;
+
+    for (int i = 0; i < strlen(word); i++)
+    {
+        if (isupper(word[i]))
+        {
+            score += POINTS[word[i] - 'A'];
+        }
+
+        else if (islower(word[i]))
+        {
+            score += POINTS[word[i] - 'a'];
+        }
+
+    }
+    return score;
 }
