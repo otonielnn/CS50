f91bd8b86ec0742b14eb283b6fd715798e96c867
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-01-23 00:21:18 +0000

    Mon, Jan 22, 2024, 9:21 PM -03:00

--- module2/lab2/scrabble.c
+++ module2/lab2/scrabble.c
@@ -1,8 +1,15 @@
 #include <cs50.h>
+#include <string.h>
 
 int main(void)
 {
     string player1 = get_string("Player 1: ");
     string player2 = get_string("Player 2: ");
-    string letters = {"A" ,"B" ,"C" ,"D",E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z}
+    string letters[] = {"A" ,"B" ,"C" ,"D" ,"E" ,"F" ,"G" ,"H" ,"I" ,"J" ,"K" ,"L" ,"M" ,"N" ,"O" ,"P" ,"Q" ,"R" ,"S" ,"T" ,"U" ,"V" ,"W" ,"X" ,"Y" ,"Z"};
+    int score[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+    for (int i = 0; i < strlen(player1); i++)
+    {
+        player1[i]
+    }
 }
