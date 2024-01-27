ec93d0c22684768206f2ba6815fc32981fca42d6
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-01-27 20:20:34 +0000

    Sat, Jan 27, 2024, 5:20 PM -03:00

--- module3/names.c
+++ module3/names.c
@@ -5,5 +5,15 @@
 int main(void)
 {
     string names[] = {"Bill", "Charlie", "Fred", "George", "Ginny", "Percy", "Ron"};
-    
+
+    for (int i = 0; i < 7; i++)
+    {
+        if (strcmp(names[i], "Ron"))
+        {
+            printf("Found\n");
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
 }
