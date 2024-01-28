928646ad8c483a4d5f8fa942f155e3eb9bb429b5
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-01-28 23:46:54 +0000

    Sun, Jan 28, 2024, 8:46 PM -03:00

--- module3/runoff.c
+++ module3/runoff.c
@@ -176,7 +176,7 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    int minvotes = voter_count;
+    int minvotes = candidates[0].votes;
 
     for (int i = 0; i < candidate_count; i++)
     {
