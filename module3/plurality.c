d0575b97b77f09b497b5b8a96f106ecdb75cbc3f
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-01-28 22:15:42 +0000

    Sun, Jan 28, 2024, 7:15 PM -03:00

--- module3/plurality.c
+++ module3/plurality.c
@@ -66,14 +66,21 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-    
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(candidates[i].name, name) == 0)
+        {
+            candidates[i].votes++;
+            return true;
+        }
+    }
     return false;
 }
 
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    // TODO
+    
     return;
 }
 
