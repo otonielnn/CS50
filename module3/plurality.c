16b5d3427523dbaa4dfa0071b63d047d68f50e56
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-01-28 22:20:16 +0000

    Sun, Jan 28, 2024, 7:20 PM -03:00

--- module3/plurality.c
+++ module3/plurality.c
@@ -80,7 +80,23 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    
+    int maxium_vote = 0;
+
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes > maxium_vote)
+        {
+            maxium_vote = candidates[i].votes;
+        }
+    }
+
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes == maxium_vote)
+        {
+            printf("%s\n", candidates[i].name);
+        }
+    }
     return;
 }
 
