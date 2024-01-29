be8f2ffa88d3f1f11de2f250e9cba1f17473247c
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-01-29 00:12:39 +0000

    Sun, Jan 28, 2024, 9:12 PM -03:00

--- module3/tideman.c
+++ module3/tideman.c
@@ -15,8 +15,7 @@ typedef struct
 {
     int winner;
     int loser;
-}
-pair;
+} pair;
 
 // Array of candidates
 string candidates[MAX];
@@ -129,15 +128,18 @@ void add_pairs(void)
     {
         for (int j = i + 1; j < candidate_count; j++)
         {
-            pairs[pair_count].winner = i;
-            pairs[pair_count].loser = j;
-            pair_count++
-        }
-        else if (preferences[i][j] < preferences[j][i])
-        {
-            pairs[pair_count].winner = j;
-            pairs[pair_count].loser = i;
-            pair_count++;
+            if (preferences[i][j] > preferences[j][i])
+            {
+                pairs[pair_count].winner = i;
+                pairs[pair_count].loser = j;
+                pair_count++
+            }
+            else if (preferences[i][j] < preferences[j][i])
+            {
+                pairs[pair_count].winner = j;
+                pairs[pair_count].loser = i;
+                pair_count++;
+            }
         }
     }
 }
@@ -162,4 +164,3 @@ void print_winner(void)
     // TODO
     return;
 }
-
