982e8ef845dd0f14f93fc29a8ede3b1269d6b99e
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-03-22 04:30:36 +0000

    Fri, Mar 22, 2024, 1:30 AM -03:00

--- module4/compare.c
+++ module4/compare.c
@@ -3,10 +3,22 @@
 
 int main(void)
 {
-    int i = get_int("i: ");
+    /* int i = get_int("i: ");
     int j = get_int("j: ");
 
     if (i == j)
+    {
+        printf("Same\n");
+    }
+    else
+    {
+        printf("Different\n");
+    } */
+
+    char *s = get_string("s: ");
+    char *t = get_string("t: ");
+
+    if (s == t)
     {
         printf("Same\n");
     }
