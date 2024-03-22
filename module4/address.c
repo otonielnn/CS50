fd00a49750c61d40eb29eeb8897c988f0df721da
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-03-22 04:18:45 +0000

    Fri, Mar 22, 2024, 1:18 AM -03:00

--- module4/address.c
+++ module4/address.c
@@ -5,4 +5,9 @@ int main(void)
     int n = 50;
     int *p = &n;
     printf("%p\n", p);
+
+    char *s = "HI!";
+    printf("%p\n", &s[0]);
+    printf("%p\n", &s[1]);
+    printf("%p\n", &s[2]);
 }
