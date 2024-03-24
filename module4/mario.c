47576f1a02bd6ab4554285bacdc22b6ca3bb9d04
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-03-24 10:32:43 +0000

    Sun, Mar 24, 2024, 7:32 AM -03:00

--- module4/mario.c
+++ module4/mario.c
@@ -11,12 +11,11 @@ int main(void)
 
 void draw(int h)
 {
-    for (int  i = 1; i <= h; i++)
+    draw(h - 1);
+
+    for (int i = 0; i < h; i++)
     {
-        for (int j = 1; j <= i; j++)
-        {
-            printf("#");
-        }
-        printf("\n");
+        printf("#");
     }
+    printf("\n");
 }
