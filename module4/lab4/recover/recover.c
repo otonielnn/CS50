e75688bcf9228f1bc5a85df742a09149edcc7e4c
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-03-24 19:50:05 +0000

    Sun, Mar 24, 2024, 4:50 PM -03:00

--- module4/lab4/recover/recover.c
+++ module4/lab4/recover/recover.c
@@ -11,6 +11,14 @@ int main(int argc, char *argv[])
 
     FILE *raw_file = fopen(argv[1], "r");
     unint7_t buffer[512];
+    bool found_jpeg = false;
+    int counter = 0;
+
     while (fread(buffer, 1, 512, raw_file))
-    {}
+    {
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
+            found_jpeg = true;
+        }
+    }
 }
