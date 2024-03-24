af85b0e81a4526078c85de25c74749749436ae96
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-03-24 18:29:12 +0000

    Sun, Mar 24, 2024, 3:29 PM -03:00

--- module4/lab4/filter-less/helpers.c
+++ module4/lab4/filter-less/helpers.c
@@ -19,7 +19,14 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int originalRed = image[i][j].rgbtRed;
+            int originalGreen = image[i][j].rgbtGreen;
+            int originalBlue = image[i][j].rgbtBlue;        }
+    }
 }
 
 // Reflect image horizontally
