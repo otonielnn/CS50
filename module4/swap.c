77325df4c0330428cbb8719ea0d95f5ab0ea7cf4
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-03-24 09:47:39 +0000

    Sun, Mar 24, 2024, 6:47 AM -03:00

---
+++ module4/swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 1;
+    int y = 2;
+    printf("X is %i, y is %i\n", x, y);
+    swap(x, y);
+    printf("X is %i, y is %i\n", x, y);
+}
+
+void swap(int a, int b)
+{
+    
+}
