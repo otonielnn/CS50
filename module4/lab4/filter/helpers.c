6c251a85cbc28e23ef6b1dacb6fcfd8f98d1f7c3
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-03-24 17:39:31 +0000

    Sun, Mar 24, 2024, 2:39 PM -03:00

--- module4/lab4/filter/helpers.c
+++ module4/lab4/filter/helpers.c
@@ -97,12 +97,15 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
             {
                 for (int updated_j = j - 1; updated_j < j + 2; updated_j++)
                 {
-                    gx_red += gx_array[counter] * image[updated_i][updated_j].rgbtRed;
-                    gx_green += gx_array[counter] * image[updated_i][updated_j].rgbtGreen;
-                    gx_blue += gx_array[counter] * image[updated_i][updated_j].rgbtBlue;
-                    gy_red += gy_array[counter] * image[updated_i][updated_j].rgbtRed;
-                    gy_green += gy_array[counter] * image[updated_i][updated_j].rgbtGreen;
-                    gy_blue += gy_array[counter] * image[updated_i][updated_j].rgbtBlue;
+                    if (!(updated_i < 0 || updated_j < 0 || updated_i > height || updated_j > width))
+                    {
+                        gx_red += gx_array[counter] * image[updated_i][updated_j].rgbtRed;
+                        gx_green += gx_array[counter] * image[updated_i][updated_j].rgbtGreen;
+                        gx_blue += gx_array[counter] * image[updated_i][updated_j].rgbtBlue;
+                        gy_red += gy_array[counter] * image[updated_i][updated_j].rgbtRed;
+                        gy_green += gy_array[counter] * image[updated_i][updated_j].rgbtGreen;
+                        gy_blue += gy_array[counter] * image[updated_i][updated_j].rgbtBlue;
+                    }
                 }
                 counter++;
             }
