d066759f838bc67777036d3c4b9ee6d8f711b5a5
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-03-24 19:57:55 +0000

    Sun, Mar 24, 2024, 4:57 PM -03:00

--- module4/lab4/recover/recover.c
+++ module4/lab4/recover/recover.c
@@ -10,15 +10,43 @@ int main(int argc, char *argv[])
     }
 
     FILE *raw_file = fopen(argv[1], "r");
-    unint7_t buffer[512];
+    unint8_t buffer[512];
     bool found_jpeg = false;
     int counter = 0;
+    char filename[8];
+    FILE *img = NULL;
 
     while (fread(buffer, 1, 512, raw_file))
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
             found_jpeg = true;
+        }
+
+        if (found_jpeg == true)
+        {
+            if (counter != 0 )
+            {
+                fclose(img);
+            }
+
+            if (counter == 0)
+            {
+                sprintf(filename, "%03i.jpg", counter);
+                img = fopen(filename, "w");
+                fwrite(buffer, 1, 512, img);
+                found_jpeg = false;
+                counter++;
+            }
+            else
+            {
+                fclose(img);
+                sprintf(filename, "%03i.jpg", counter);
+                img = fopen(filename, "w");
+                fwrite(buffer, 1, 512, img);
+                found_jpeg = false;
+                counter++;
+            }
         }
     }
 }
