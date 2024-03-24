39bddc988bea433eae47575eaabb1ca94e089cc6
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-03-24 23:01:05 +0000

    Sun, Mar 24, 2024, 8:01 PM -03:00

--- module4/lab4/volume.c
+++ module4/lab4/volume.c
@@ -39,7 +39,7 @@ int main(int argc, char *argv[])
     fwrite(header_buffer, sizeof(uint8_t), HEADER_SIZE, output);
 
     // TODO: Read samples from input file and write updated data to output file
-    int16_t
+    int16_t buffer
 
     // Close files
     fclose(input);
