2ae4541793719d3226e49c6174621934413249d5
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-03-27 00:22:02 +0000

    Tue, Mar 26, 2024, 9:22 PM -03:00

--- module5/list.c
+++ module5/list.c
@@ -7,4 +7,30 @@ int main(void)
     {
         return 1;
     }
+    list[0] = 1;
+    list[1] = 2;
+    list[2] = 3;
+
+    int *tmp = malloc(4 * sizeof(int));
+    if (tmp == NULL)
+    {
+        free(list);
+        return 1;
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        tmp[i] = list[i];
+    }
+
+    tmp[3] = 4;
+
+    free(list);
+
+    list = tmp;
+
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%i\n", list[i]);
+    }
 }
