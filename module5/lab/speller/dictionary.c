6d8ebb51b0f51cbd3ea54afdb8464221fef7b0d3
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-05-20 15:11:25 +0000

    Mon, May 20, 2024, 12:11 PM -03:00

--- module5/lab/speller/dictionary.c
+++ module5/lab/speller/dictionary.c
@@ -1,11 +1,6 @@
 // Implements a dictionary's functionality
 
-#include <ctype.h>
 #include <stdbool.h>
-#include <strings.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
 
 #include "dictionary.h"
 
@@ -23,24 +18,10 @@ const unsigned int N = 1;
 // Hash table
 node *table[N];
 
-unsigned int hash_value;
-unsigned int word_count;
-
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
     // TODO
-    hash_value = hash(word);
-    node *cursor = table[hash_value];
-
-    while (cursor != 0)
-    {
-        if (strcasecmp(word, cursor->word) == 0)
-        {
-            return true;
-        }
-        cursor = cursor->next;
-    }
     return false;
 }
 
@@ -48,54 +29,20 @@ bool check(const char *word)
 unsigned int hash(const char *word)
 {
     // TODO
-    unsigned long total = 0;
-    for (int i = 0; i < strlen(word); i++)
-    {
-        total += tolower(word[i]);
-    }
-    return total % N;
+    return 0;
 }
 
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
     // TODO
-    FILE *file = fopen(dictionary, "r");
-
-    if (file == NULL)
-    {
-        printf("Unable to open %s\n", dictionary);
-        return false;
-    }
-
-    char word[LENGTH + 1];
-    while (fscanf(file, "%s", word) != EOF)
-    {
-        node *n = malloc(sizeof(node));
-
-        if (n == NULL)
-        {
-            return false;
-        }
-
-        strcpy(n->word, word);
-        hash_value = hash(word);
-        n->next = table[hash_value];
-        table[hash_value] = n;
-        word_count++;
-    }
-    fclose(file);
-    return true;
+    return false;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
     // TODO
-    if (word_count > 0)
-    {
-        return word_count;
-    }
     return 0;
 }
 
@@ -103,15 +50,5 @@ unsigned int size(void)
 bool unload(void)
 {
     // TODO
-    for (int i = 0; i < N; i++)
-    {
-        node *cursor = table[i];
-        while (cursor)
-        {
-            node *tmp = cursor;
-            cursor = cursor->next;
-            free(tmp);
-        }
-    }
-    return true;
+    return false;
 }
