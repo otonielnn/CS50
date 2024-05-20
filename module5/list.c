3e61325c848b7442653fae770a38d64d3b442b2c
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-05-20 01:31:17 +0000

    Sun, May 19, 2024, 10:31 PM -03:00

--- module5/list.c
+++ module5/list.c
@@ -13,47 +13,41 @@ int main(void)
     node *list = NULL;
 
     node *n = malloc(sizeof(node));
-
     if (n == NULL)
     {
         return 1;
     }
-
-    n->number = 1;
+    n-> number = 1;
     n->next = NULL;
     list = n;
 
     n = malloc(sizeof(node));
-
     if (n == NULL)
     {
         free(list);
         return 1;
     }
-
     n->number = 2;
     n->next = NULL;
     list->next = n;
 
     n = malloc(sizeof(node));
-
     if (n == NULL)
     {
-        free(list->next);
+        free(list->next);c
         free(list);
         return 1;
     }
-
     n->number = 3;
     n->next = NULL;
     list->next->next = n;
 
-    for (node *tmp = list; tmp != null; tmp = tmp->next)
+    for(node *tmp = list; tmp != NULL; tmp = tmp->next)
     {
         printf("%i\n", tmp->number);
     }
 
-    while (list != NULL)
+    while(list != NULL)
     {
         node *tmp = list->next;
         free(list);
