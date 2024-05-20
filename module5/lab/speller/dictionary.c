9f2350428b4d79d35afc7389584720d55db7d810
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-05-20 02:14:05 +0000

    Sun, May 19, 2024, 11:14 PM -03:00

--- module5/lab/speller/dictionary.c
+++ module5/lab/speller/dictionary.c
@@ -29,7 +29,7 @@ unsigned int word_count;
 bool check(const char *word)
 {
     // TODO
-    hash_value * hash(word);
+    hash_value = hash(word);
     node *cursor = table[hash_value];
 
     while (cursor != 0)
