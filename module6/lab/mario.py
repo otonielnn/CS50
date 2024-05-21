6b670d844ae8e616ab9ae781322677d735fb717f
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-05-21 19:02:37 +0000

    Tue, May 21, 2024, 4:02 PM -03:00

--- module6/lab/mario.py
+++ module6/lab/mario.py
@@ -1,7 +1,11 @@
 n = 0
 
 while n < 1 or n > 8:
-    n = int(input("Height: "))
+    try:
+        n = int(input("Height: "))
+    except ValueError:
+        print("Write a positive Integer number between 1 and 8")
+
 
 for i in range(1, n + 1):
     for j in range(n - i):
