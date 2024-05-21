49a02457c119a4f3bdede043823737e03470ea9f
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-05-21 16:54:45 +0000

    Tue, May 21, 2024, 1:54 PM -03:00

--- module6/lab/mario.py
+++ module6/lab/mario.py
@@ -3,16 +3,12 @@ n = 0
 while n < 1 or n > 8:
     n = int(input("Height: "))
 
-def torre(n):
-    for i in range(n):
+for i in range(n):
+    j = n
+    while j > n:
+        if n != 0:
+            print(" ")
 
-        j = n
-        while j > n:
-            if n != 0:
-                print(" ")
-
-        h = 1
-        for h in range(n):
-            print("#", end=" ")
-
-        print()
+    h = 1
+    for h in range(n):
+        print("#", end=" ")
