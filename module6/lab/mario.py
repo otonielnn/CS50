4b6182b285a66baf98bcdf91c2b228e16960e403
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-05-21 17:13:50 +0000

    Tue, May 21, 2024, 2:13 PM -03:00

--- module6/lab/mario.py
+++ module6/lab/mario.py
@@ -6,7 +6,7 @@ while n < 1 or n > 8:
 for i in range(1, n + 1):
     for j in range(n - i):
         if i != 0:
-            print(" ", end="")
+            print(end=" ")
 
     for h in range(i):
         print("#", end="")
