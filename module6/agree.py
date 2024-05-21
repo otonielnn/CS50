6030478685eee96109cbc9dc795f42695e80a96e
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-05-21 02:17:23 +0000

    Mon, May 20, 2024, 11:17 PM -03:00

--- module6/agree.py
+++ module6/agree.py
@@ -2,7 +2,7 @@ from cs50 import get_string
 
 s = get_string("Do you agree? ")
 
-if s.lower == "y":
+if s.lower in ["y", "yes"]:
     print("Agreed.")
-elif s.lower == "n":
+elif s.lower in ["n", "no"]:
     print("Not Agreed")
