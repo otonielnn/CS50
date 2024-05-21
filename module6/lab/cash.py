f3fbbed4dda7c95ae26df982cdd1aca59ec15e18
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-05-21 19:21:27 +0000

    Tue, May 21, 2024, 4:21 PM -03:00

--- module6/lab/cash.py
+++ module6/lab/cash.py
@@ -1,1 +1,23 @@
-change = float(input("Changed owed: "))
+change = -1
+
+while change < 0:
+    change = float(input("Changed owed: "))
+
+int cents = round(change * 100)
+coins = 0
+
+while cents > 0;
+    if cents >= 25:
+        cents -= 25
+        coins += 1
+    elif cents >= 10:
+        cents -= 10
+        coins += 1
+    elif cents >= 5:
+        cents -= 5
+        coins += 1
+    else:
+        cents -= 1
+        coins += 1
+
+print(coins)
