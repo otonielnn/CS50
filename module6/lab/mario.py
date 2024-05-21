427842ddd55be7c20b632e368650807bd4804c45
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-05-21 17:13:17 +0000

    Tue, May 21, 2024, 2:13 PM -03:00

--- module6/lab/mario.py
+++ module6/lab/mario.py
@@ -3,15 +3,12 @@ n = 0
 while n < 1 or n > 8:
     n = int(input("Height: "))
 
-for i in range(n + 1):
-    j = n
-    for j in range(j ,i , -1):
+for i in range(1, n + 1):
+    for j in range(n - i):
         if i != 0:
             print(" ", end="")
 
-
-    h = 1
     for h in range(i):
-        print("#", end=" ")
+        print("#", end="")
 
     print()
