17416455f40df9a34a50f87ac8c67b2fc6b9a345
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-05-21 01:21:56 +0000

    Mon, May 20, 2024, 10:21 PM -03:00

--- module6/hello.py
+++ module6/hello.py
@@ -2,3 +2,8 @@ from cs50 import get_string
 
 answer = get_string("What's your name? ")
 print(f"Hello, {answer}")
+
+# input nativo do python
+
+answer = input("What's your name? ")
+print(f"Hello, {answer}")
