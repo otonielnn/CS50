7e9429a6c93e7fc478a6e025a40ab122139217b7
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-06-11 21:47:33 +0000

    Tue, Jun 11, 2024, 6:47 PM -03:00

--- module7/lab/fiftyville/log.sql
+++ module7/lab/fiftyville/log.sql
@@ -6,13 +6,16 @@ SELECT * FROM courthouse_security_logs
 WHERE day = 28 and hour = 10;
 
 SELECT * FROM people
-WHERE license_plate = "13FNH73";
+WHERE license_plate = "5P2BI95";
+
+SELECT * FROM phone_calls
+WHERE caller = "(725) 555-4692" OR receiver = "(725) 555-4692";
 
 SELECT * FROM interviews
 WHERE day = 28 AND month = 7;
 
 SELECT * FROM passengers
-WHERE passport_number = "3642612721";
+WHERE passport_number = "2963008352";
 
 SELECT * FROM flights
 WHERE day = 28 and month = 7;
