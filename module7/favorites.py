82e299e20bce5b64ab863ada1d75a0b1d2c59120
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-06-11 15:46:03 +0000

    Tue, Jun 11, 2024, 12:46 PM -03:00

--- module7/favorites.py
+++ module7/favorites.py
@@ -8,6 +8,7 @@ open("show.db", "w").close()
 db = SQL("sqlite:///shows.db")
 
 db.execute("CREATE TABLE shows (id INTEGER, title TEXT, PRIMARY KEY(id))")
+db.execute("CREATE TABLE genres (show_id INTEGER, genre TEXT, FOREIGN KEY(show_id) REFERENCES shows(id)")
 
 with open("Favorite TV Shows - Form Responses 1.csv", "r") as file:
 
