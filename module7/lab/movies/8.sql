b44a540fb7d2c017af183d10226ab10f39ec1720
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-06-11 18:58:18 +0000

    Tue, Jun 11, 2024, 3:58 PM -03:00

--- module7/lab/movies/8.sql
+++ module7/lab/movies/8.sql
@@ -1,3 +1,4 @@
 SELECT name FROM people
 JOIN stars ON people.id = stars.person_id
-JOIN movies ON stars.person_id = movies.id;
+JOIN movies ON stars.person_id = movies.id
+WHERE movies.title = "Toy Story";
