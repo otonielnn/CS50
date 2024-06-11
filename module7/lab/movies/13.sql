5f2c8662520004024867dc3e9b93a48d3fc895e8
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-06-11 20:26:21 +0000

    Tue, Jun 11, 2024, 5:26 PM -03:00

---
+++ module7/lab/movies/13.sql
@@ -0,0 +1,9 @@
+SELECT DISTINCT(name) FROM people
+JOIN stars ON people.id = stars.person_id
+JOIN movies ON movies.id = stars.movie_id
+WHERE movies.title IN (
+    SELECT DISTINCT(movies.title) FROM people
+    JOIN stars ON people.id = stars.person_id
+    JOIN movies ON movies.id = stars.movie_id
+    WHERE people.name = "Kevin Bacon)
+);
