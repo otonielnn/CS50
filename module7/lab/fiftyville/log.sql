708875fd63941a7c4d4cc88ed04e0d5969413e37
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-06-11 21:12:51 +0000

    Tue, Jun 11, 2024, 6:12 PM -03:00

--- module7/lab/fiftyville/log.sql
+++ module7/lab/fiftyville/log.sql
@@ -1,3 +1,6 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 SELECT * FROM crime_scene_reports
 WHERE street = "Chamberlin Street";
+
+SELECT * FROM courthouse_security_logs
+WHERE day = 28 and hour = 10;
