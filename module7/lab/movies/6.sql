bcc1752775a1860c9e58969b36ba8711a807b4c7
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-06-11 18:35:30 +0000

    Tue, Jun 11, 2024, 3:35 PM -03:00

--- module7/lab/movies/6.sql
+++ module7/lab/movies/6.sql
@@ -1,1 +1,1 @@
-SELECT ROUND(AVG(rating), 2) FROM ratings INNER JOIN movies ON ratings.movie_id = movies.id;
+SELECT ROUND(AVG(rating), 2) FROM ratings INNER JOIN movies ON ratings.movie_id = movies.id WHERE year = 2012;
