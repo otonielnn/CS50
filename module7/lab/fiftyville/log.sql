45432ac0ee1f38db41720f03db2a14a3f24f0c22
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-06-11 22:51:53 +0000

    Tue, Jun 11, 2024, 7:51 PM -03:00

--- module7/lab/fiftyville/log.sql
+++ module7/lab/fiftyville/log.sql
@@ -4,13 +4,13 @@ WHERE street = "Chamberlin Street" AND description LIKE "%cs50%";
 
 SELECT * FROM interviews
 WHERE day = 28 AND month = 7 AND transcript like "%courthouse%";
-
+-- Witness RUTH
 SELECT * FROM courthouse_security_logs
 WHERE day = 28 AND hour = 10 AND activity = "exit" AND minute BETWEEN 15 AND 25;
-
+-- Witness Eugene
 SELECT * FROM atm_transactions
-WHERE atm_location = "Fifer Street" AND transaction_type = "withdraw" AND month = 7 AND day = 28 AND account_number = "28500762";
-
+WHERE atm_location = "Fifer Street" AND transaction_type = "withdraw" AND month = 7 AND day = 28;
+-- Witness Raymond
 SELECT * FROM phone_calls
 WHERE duration = 60 AND month = 7 AND day = 28;
 
