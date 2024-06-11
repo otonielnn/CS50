1983685161dea5d87f195ad377d5565967b15c4b
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-06-11 21:57:30 +0000

    Tue, Jun 11, 2024, 6:57 PM -03:00

--- module7/lab/fiftyville/log.sql
+++ module7/lab/fiftyville/log.sql
@@ -2,6 +2,9 @@
 SELECT * FROM crime_scene_reports
 WHERE street = "Chamberlin Street" AND description LIKE "%cs50%";
 
+SELECT * FROM interviews
+WHERE day = 28 AND month = 7;
+
 SELECT * FROM courthouse_security_logs
 WHERE day = 28 and hour = 10;
 
@@ -11,8 +14,6 @@ WHERE license_plate = "5P2BI95";
 SELECT * FROM phone_calls
 WHERE caller = "(725) 555-4692" OR receiver = "(725) 555-4692";
 
-SELECT * FROM interviews
-WHERE day = 28 AND month = 7;
 
 SELECT * FROM passengers
 WHERE passport_number = "2963008352";
