7444a1513cfd44299f76a11565866bc5ba6cc075
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-06-11 20:29:40 +0000

    Tue, Jun 11, 2024, 5:29 PM -03:00

--- module7/lab/movies/13.sql
+++ module7/lab/movies/13.sql
@@ -1,7 +1,7 @@
 SELECT DISTINCT(name) FROM people
 JOIN stars ON people.id = stars.person_id
 JOIN movies ON movies.id = stars.movie_id
-WHERE movies.title IN (
+WHERE name != "Kevin Bacon" and movies.title IN (
     SELECT DISTINCT(movies.title) FROM people
     JOIN stars ON people.id = stars.person_id
     JOIN movies ON movies.id = stars.movie_id
