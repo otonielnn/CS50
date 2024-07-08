2bed1f7f615feebfb43c4378f6d847a2a71ad2b6
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-07-08 18:37:24 +0000

    Mon, Jul 8, 2024, 3:37 PM -03:00

--- module9/hello/application.py
+++ module9/hello/application.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html", name=request.args.get("name"))
+
+@app.router("/greet")
+def greet():
+    return render_template("greet.html")
