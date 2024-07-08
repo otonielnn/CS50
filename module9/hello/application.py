35e35ce5227e98677d9e4d1799e268d885989203
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-07-08 18:55:03 +0000

    Mon, Jul 8, 2024, 3:55 PM -03:00

--- module9/hello/application.py
+++ module9/hello/application.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
 
-@app.route("/greet")
+@app.route("/greet", methods=["POST"])
 def greet():
-    return render_template("greet.html", name=request.args.get("name", "World"))
+    return render_template("greet.html", name=request.form.get("name", "World"))
