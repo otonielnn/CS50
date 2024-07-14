ae28d040e5a2d5e6de7774f59f42e84e16c645ba
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-07-14 21:09:33 +0000

    Sun, Jul 14, 2024, 6:09 PM -03:00

--- module9/froshims/application.py
+++ module9/froshims/application.py
@@ -20,7 +20,9 @@ def index():
 def register():
     name = request.form.get("name")
     if not name:
-        return render_template("failure.html")
+        return render_template("error.html", message="Missing name")
     sport = request.form.get("sport")
     if not sport:
-        return render_template("failure.html")
+        return render_template("error.html", message="Missing sport")
+    if sport not in SPORTS:
+        return render_template("error.html", message="Invalid sport")
