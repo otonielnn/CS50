34ba2c91921c74ca9db1098087bb352da12e4fe1
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-07-14 03:15:54 +0000

    Sun, Jul 14, 2024, 12:15 AM -03:00

--- module9/froshims/application.py
+++ module9/froshims/application.py
@@ -2,9 +2,17 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+SPORTS = [
+    "Dodgeball",
+    "Flag Football",
+    "Soccer",
+    "Volleyball",
+    "Ultimate Frisbee"
+]
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", sports=SPORTS)
 
 @app.route("/register", methods=["POST"])
 def register():
