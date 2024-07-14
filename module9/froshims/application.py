301b6f318db7c8c228c3791eaaced17e7bfc58a4
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-07-14 03:04:42 +0000

    Sun, Jul 14, 2024, 12:04 AM -03:00

--- module9/froshims/application.py
+++ module9/froshims/application.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/register")
+@app.route("/register" methods=["POST"])
 def register():
     return render_template("success.html")
