7ebbc390ec206c8c1895e1cd4a1b03c5ddc99196
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-07-14 03:21:48 +0000

    Sun, Jul 14, 2024, 12:21 AM -03:00

--- module9/froshims/application.py
+++ module9/froshims/application.py
@@ -16,6 +16,6 @@ def index():
 
 @app.route("/register", methods=["POST"])
 def register():
-    if not request.form.get("name") or request.form.get("sport"):
+    if not request.form.get("name") or request.form.get("sport") not in SPORTS:
         return render_template("failure.html")
     return render_template("success.html")
