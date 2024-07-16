e5a190cb3de7bd6e2cbe2dd3bbcbc1ba22aa1e91
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-07-16 19:10:23 +0000

    Tue, Jul 16, 2024, 4:10 PM -03:00

--- module9/froshims/application.py
+++ module9/froshims/application.py
@@ -1,7 +1,14 @@
+import os
 from cs50 import SQL
 from flask import Flask, render_template, request, redirect
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
+app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_DEFAULT_SENDER")
+app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
 
 db = SQL("sqlite:///froshims.db")
 
