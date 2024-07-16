bc5b558a430893b7614dce51e5ed6a532fed7d6a
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-07-16 19:20:02 +0000

    Tue, Jul 16, 2024, 4:20 PM -03:00

--- module9/froshims/application.py
+++ module9/froshims/application.py
@@ -8,6 +8,7 @@ app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_DEFAULT_SENDER")
 app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
 mail = Mail(app)
 
