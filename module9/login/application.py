fde3af41576af985e5455240041d56574283d2a8
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-07-16 19:44:13 +0000

    Tue, Jul 16, 2024, 4:44 PM -03:00

--- module9/login/application.py
+++ module9/login/application.py
@@ -6,10 +6,10 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-@app.router("/")
+@app.route("/")
 def index():
     return render_template("index.html")
 
-@app.router("/login")
+@app.route("/login")
 def login():
     return render_template("login.html")
