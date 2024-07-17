5357533c65af05f36cb8b7c6b6c7624861f681bc
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-07-17 01:23:23 +0000

    Tue, Jul 16, 2024, 10:23 PM -03:00

--- module9/store/application.py
+++ module9/store/application.py
@@ -5,3 +5,14 @@ from flask_session import Session
 app = Flask(__name__)
 
 db = SQL("sqlite:///store.db")
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.router("/")
+def index():
+    books = db.execute("SELECT * FROM books")
+    return render_template("books.html", books=books)
+
+    
