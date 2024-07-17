fcf2d157b4ba07502c9176940ee834642e5def88
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-07-17 01:31:25 +0000

    Tue, Jul 16, 2024, 10:31 PM -03:00

--- module9/store/application.py
+++ module9/store/application.py
@@ -10,7 +10,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-@app.router("/")
+@app.route("/")
 def index():
     books = db.execute("SELECT * FROM books")
     return render_template("books.html", books=books)
